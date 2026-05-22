8e435cc13ba0e50e918364ce3c04329fda7c9a34
Samir Morhill <Samir.Morhill@alu.frt.utn.edu.ar>
2026-05-22 09:18:18 -0300

    CasoDictionary realizado.

--- Dsw2026Ej11/Collections/CasoDictionary.cs
+++ Dsw2026Ej11/Collections/CasoDictionary.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un diccionario donde la clave sea el legajo y el valor el alumno
 //Incluir un método para agregar un alumno al diccionario
@@ -7,4 +9,39 @@
 //Incluir un método para eliminar un alumno utilizando la clave
 public class CasoDictionary
 {
+    private Dictionary<int, Alumno> Alumnos { get; set; } = new Dictionary<int, Alumno>();
+
+    public void AgregarAlumno(Alumno alumno)
+    {
+        if (!Alumnos.ContainsKey(alumno.Legajo))
+        {
+            Alumnos.Add(alumno.Legajo, alumno);
+        }
+    }
+
+    public Alumno? ObtenerUnAlumno(int legajo)
+    {
+        if(Alumnos.TryGetValue(legajo, out Alumno? alumno))
+        {
+            return alumno;
+        }
+        return null;
+
+    }
+
+    public Dictionary<int, Alumno> RetornarAlumnos()
+    {
+        return Alumnos;
+    }
+
+    public void EliminarAlumno(int legajo)
+    {
+        if (Alumnos.ContainsKey(legajo))
+        {
+            Alumnos.Remove(legajo);
+        }
+            
+    }
+
+   
 }
--- Dsw2026Ej11/Domain/Alumno.cs
+++ Dsw2026Ej11/Domain/Alumno.cs
@@ -3,12 +3,14 @@
 public class Alumno
 {
     public int Id { get; }
+    public int Legajo { get; }
     public string Nombre { get; }
     public double Promedio { get; }
 
-    public Alumno(int id, string nombre, double promedio)
+    public Alumno(int id, int legajo, string nombre, double promedio)
     {
         Id = id;
+        Legajo = legajo;
         Nombre = nombre;
         Promedio = promedio;
     }
